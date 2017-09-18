e2c788e2fdc25d16f3b19df9f6d1617492795994
Rhathe <rassinator@gmail.com>
2017-09-18 14:18:32 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,14 +4,21 @@ defmodule Cingi.Mixfile do
 	def project do
 		[
 			app: :cingi,
+			name: "CingI",
 			version: "0.1.0",
 			elixir: "~> 1.5",
 			escript: [
 				main_module: Cingi.CLI,
 				emu_args: "-noinput",
 			],
+			source_url: "https://github.com/Rhathe/CingI",
 			start_permanent: Mix.env == :prod,
-			deps: deps()
+			deps: deps(),
+			package: package(),
+			description: """
+			Continuous-ing Integration (...core). A distributed pipeline-based
+			command line task runner providing the core functionality for a CI server.
+			""",
 		]
 	end
 
@@ -35,6 +42,15 @@ defmodule Cingi.Mixfile do
 			{:yaml_elixir, "~> 1.3.1"},
 			{:temp, "~> 0.4"},
 			{:gproc, "~> 0.5.0"},
+		]
+	end
+
+	defp package do
+		[
+			maintainers: ["Ramon Sandoval"],
+			licenses: ["MIT"],
+			links: %{github: "https://github.com/Rhathe/CingI"},
+			files: ~w(lib priv README.md config c-ing-i-logo.svg LICENSE),
 		]
 	end
 end
